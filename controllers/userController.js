0062adb5d5f7d424d4635a75adecfe6647161632
Slade Sawyer <Slade@Slades-MacBook-Air.local>
2019-05-14 16:24:22 -0400

    rename connections, comment out controller syntax for heroku error

--- controllers/userController.js
+++ controllers/userController.js
@@ -26,17 +26,19 @@ const userController = {
         )
     },
 
-    updateUser: function(req,res){
-        User.findByIdAndUpdate(req.params.userId, {userName = req.body.userName}, {new:true}).then( () => {
-            res.redirect('/allUsers')
-        });
-    },
+    // updateUser: function(req,res){
+    //     User.findByIdAndUpdate(req.params.userId,
+    //                          {userName = req.body.userName}, 
+    //                         {new:true})
+    //         .then( () => {res.redirect('/allUsers')
+    //     });
+    // },
 
-    deleteUser: function(req,res){
-        User.findByIdAndDelete(req.params.userId).then( () => {
-            res.redirect('/allUsers')
-        })
-    }
+    // deleteUser: function(req,res){
+    //     User.findByIdAndDelete(req.params.userId).then( () => {
+    //         res.redirect('/allUsers')
+    //     })
+    // }
 }
 
 module.exports = userController;
