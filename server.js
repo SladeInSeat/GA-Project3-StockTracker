dcf0083d55b24d3db3546ebb22bc28d732ae0b0a
Slade Sawyer <Slade@Slades-MacBook-Air.local>
2019-05-16 13:05:23 -0400

    created react-app in client folder, added morgan, concurrently, axios ,styled-components, react-router-dom, added proxy to client/package.json, added express static middleware to server

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express')
+const logger = require('morgan')
 const app = express()
 const routes = require('./routes/index')
 
+app.use(logger('dev'))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+app.use(express.static(`${__dirname}/client/build`))
+
 app.use('/', routes)
+
+app.get('/*', (req, res) => {
+    res.sendFile(`${__dirname}/client/build/index.html`)
+  })
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
