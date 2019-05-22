509502231eceaf1ccd527543a2a688ad7286edaa
Slade Sawyer <Slade@Slades-MacBook-Air.local>
2019-05-22 18:51:25 -0400

    seeds refactored using async await

--- db/seeds.js
+++ db/seeds.js
@@ -1,45 +1,50 @@
 const User = require('../models/User.js')
 const Account = require('../models/Account.js')
 const Stock = require('../models/Stock.js')
+const mongoose = require('mongoose')
 
+async function buildSeeds() {
+    await User.deleteMany()
+    let createdUsers = await User.create({ userName: "Slade" }, { userName: "Sam" })
+    console.log(createdUsers)
 
-User.deleteMany().then(() => {
-    User.create([
-        {userName: "Slade"},
-        {userName: "Sam"}
-    ]).then((users) => 
-            {console.log("made users: " + users)})
-});
+    await Account.deleteMany()
+    let createdAccounts = await Account.create(
+        { accountName: "Slades Account", parentUser: createdUsers[0]._id },
+        { accountName: "Sams Account", parentUser: createdUsers[1]._id });
+    console.log(createdAccounts)
 
-Account.deleteMany().then(() => {
-    User.find().then( (foundUsers) => {
-        Account.create([{accountName: "Slades Account",
-                        parentUser: foundUsers[0]._id},
-                        {accountName: "Sams Account",
-                        parentUser: foundUsers[1]._id}])
-    }).then( (newAccounts) => {console.log("created accounts:" + newAccounts)})
-    .then(() => {
-        Stock.deleteMany().then(() => {
-            Account.find().then( (accounts) => {
-                Stock.create([{stockName: "Unilever",
-                                stockTicker: "UL",
-                                price: 59.56,
-                                parentAccount: accounts[0]._id},
-                                {stockName: "Apple",
-                                stockTicker: "AAPL",
-                                price: 190.92,
-                                parentAccount: accounts[0]._id},
-                                {stockName: "Pipestone Inc",
-                                stockTicker: "BKBEF",
-                                price: 1.32,
-                                parentAccount: accounts[1]._id},
-                               {stockName: "Rio Tinto",
-                                stockTicker: "RIO",
-                                price: 58.20,
-                                parentAccount: accounts[1]._id}]
-                ).then( () => {console.log("created stocks")})
-            })
-        })
-    })
-});
+    await Stock.deleteMany()
+    let createdStocks = await Stock.create(
+        {
+            stockName: "Unilever",
+            stockTicker: "UL",
+            price: 59.56,
+            parentAccount: createdAccounts[0]._id
+        },
+        {
+            stockName: "Apple",
+            stockTicker: "AAPL",
+            price: 190.92,
+            parentAccount: createdAccounts[0]._id
+        },
+        {
+            stockName: "Pipestone Inc",
+            stockTicker: "BKBEF",
+            price: 1.32,
+            parentAccount: createdAccounts[1]._id
+        },
+        {
+            stockName: "Rio Tinto",
+            stockTicker: "RIO",
+            price: 58.20,
+            parentAccount: createdAccounts[1]._id
+        });
+
+    console.log(createdStocks)
+
+    await mongoose.disconnect()
+}
+
+buildSeeds();
 
